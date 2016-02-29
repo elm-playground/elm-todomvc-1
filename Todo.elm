e8ec325ed637b429906e657ff7c49effb51cbe46
Dwayne R. Crooks <me@dwaynecrooks.com>
2016-02-29 05:52:00 -0400

    Add an input field to change who we greet

--- Todo.elm
+++ Todo.elm
@@ -1,5 +1,24 @@
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
+view : Signal.Address String -> String -> Html
+view address name =
+  div []
+    [ input
+        [ on "input" targetValue <| Signal.message address
+        , value name
+        , autofocus True
+        ]
+        []
+    , text <| "Hello, " ++ name ++ "!"
+    ]
+
+
+main : Signal Html
 main =
-  text "Hello, world!"
+  let
+    inbox = Signal.mailbox ""
+  in
+    Signal.map (view inbox.address) inbox.signal
